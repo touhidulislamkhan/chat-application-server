4448b15993a771679da4fcab7d3ba25a311bcf7f
touhidulislamkhan <touhidikhan0@gmail.com>
2021-10-01 19:22:27 +0600

    message receiving from client

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const http = require('http');
 const cors = require('cors');
 const router = require('./router');
 
-const {
-addUser, removeUser, getUser, getUserInRoom,
-} = require('./users');
+const { addUser, removeUser, getUser, getUserInRoom } = require('./users');
 
 const PORT = process.env.PORT || 5000;
 
@@ -19,7 +17,7 @@ const io = socketio(server, {
     cors: {
         origin: 'http://localhost:3000',
         methods: ['GET', 'POST'],
-      },
+    },
 });
 
 app.use(router);
@@ -31,8 +29,14 @@ io.on('connection', (socket) => {
 
         if (error) return callback(error);
 
-        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` });
-        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+        socket.emit('message', {
+            user: 'admin',
+            text: `${user.name}, welcome to the room ${user.room}`,
+        });
+        socket.broadcast.to(user.room).emit('message', {
+            user: 'admin',
+            text: `${user.name} has joined!`,
+        });
 
         socket.join(user.room);
 
